589ed61e9aea557f0f9813311e74c97315e860ec
milan-petrovic <milanpdo@gmail.com>
2019-02-20 10:22:12 +0100

    Optimized imports

--- app/src/main/java/com/example/android/moviedbapp/details/DetailsActivity.java
+++ app/src/main/java/com/example/android/moviedbapp/details/DetailsActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,8 +15,6 @@ import com.example.android.moviedbapp.Constants;
 import com.example.android.moviedbapp.R;
 import com.example.android.moviedbapp.Util;
 import com.squareup.picasso.Picasso;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
